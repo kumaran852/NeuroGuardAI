1e0fe50bf94346db8672733c93b381b3cd6b74d8
kumaran852 <kumaran.s2409@gmail.com>
2026-03-01 15:39:49 +0530

    Create ToolsActivity.kt with tools info activity

---
+++ android/app/src/main/java/com/neurogard/ToolsActivity.kt
@@ -0,0 +1,12 @@
+package com.neurogard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ToolsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tools)
+        // Add your tools info implementation here
+    }
+}
